1b5ffe09063a0fe4f841dafd4ea3a75ee11bed10
nekt2111 <54265542+nekt2111@users.noreply.github.com>
2020-08-03 15:31:08 +0300

    update

--- SoundOfAnimals/app/src/main/java/com/example/soundofanimals/MainActivity.java
+++ SoundOfAnimals/app/src/main/java/com/example/soundofanimals/MainActivity.java
@@ -2,36 +2,62 @@ package com.example.soundofanimals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private ImageView logo;
+    private static final String TAG = "" ;
+    private ImageView dog,monkey;
+    private MediaPlayer mp;
+    Random random;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        random = new Random();
 
-        textView = findViewById(R.id.apple_text);
-        logo = findViewById(R.id.apple_logo);
 
-        textView.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha_anim));
-        logo.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo_anim));
+        dog = findViewById(R.id.dog);
+        monkey = findViewById(R.id.monkey);
 
-        final MediaPlayer mp = MediaPlayer.create(this,R.raw.sample);
 
-        logo.setOnClickListener(new View.OnClickListener() {
+
+        dog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logo.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo_anim));
+                dog.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up_down_anim));
+                mp = MediaPlayer.create(MainActivity.this,R.raw.dog);
+                mp.start();
+            }
+        });
+        monkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                monkey.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce_anim));
+                int randomSound = random.nextInt(3);
+                Log.d(TAG, "onClick: " + randomSound);
+
+                switch (randomSound){
+                    case 0:
+                        mp = MediaPlayer.create(MainActivity.this,R.raw.monkey0);
+                        break;
+                    case 1:
+                        mp = MediaPlayer.create(MainActivity.this,R.raw.monkey1);
+                        break;
+                    case 2:
+                        mp = MediaPlayer.create(MainActivity.this,R.raw.monkey2);
+                        break;
+                }
                 mp.start();
             }
         });
